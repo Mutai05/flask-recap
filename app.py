8bb7edd5a3a208a7578498493a88dd92bd695b0a
Mutai05 <kelvok2014@gmail.com>
2024-07-27 23:20:27 +0300

    Routes, templates and html

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    return "Hello, Flask"
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about-us.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
